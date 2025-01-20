a757259632ad6b3b5b9070c8f3a71744c15901c7
Zätha <zathamalmberg@gmail.com>
2025-01-20 15:55:49 +0100

    working on making the ball bounce of the bord

--- Block_Kuzushi.c
+++ Block_Kuzushi.c
@@ -18,27 +18,31 @@ int main(void)
     Vector2 BordPosition = { 450, 600};
     
     Vector2 BallPosition = { 525, 570};
-    Vector2 BallSpeed = { -5, -5};
-    
+    Vector2 BallSpeed = { -5, -5};  
         
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        Rectangle Bord = { BordPosition.x, BordPosition.y, 150, 30};
+        Rectangle Ball = {BallPosition.x, BallPosition.y, 10, 10}; 
+        
+        bool Collition = false;
+
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
         
-        if(BordPosition.x > 0){
+        // bool CheckCollisionCircleRec(Vector2 center, float radius, Rectangle rec);
+        
+        Collition = CheckCollisionRecs(Bord, Ball);
+
+        
+        if(BordPosition.x > 0){ // moves the board
             if (IsKeyDown(KEY_A)){
                 BordPosition.x -= 6;
             }
         }
         
-        if(BordPosition.x < 850){
+        if(BordPosition.x < 850){ // moves the board
             if (IsKeyDown(KEY_D)){
                 BordPosition.x += 6;
             }
@@ -47,36 +51,44 @@ int main(void)
         BallPosition.x += BallSpeed.x;
         BallPosition.y += BallSpeed.y;
         
-        //if ((ballPosition.x >= (GetScreenWidth() - ballRadius)) || (ballPosition.x <= ballRadius)) ballSpeed.x *= -1.0f;
-        //if ((ballPosition.y >= (GetScreenHeight() - ballRadius)) || (ballPosition.y <= ballRadius)) ballSpeed.y *= -1.0f;
-        
-        
-        if((BallPosition.x >= (GetScreenWidth() - 10)) || (BallPosition.x <= 10)){
+        if((BallPosition.x >= (GetScreenWidth() - 10)) || (BallPosition.x <= 10)){ // bounce the ball in the oposite direction
             BallSpeed.x *= -1.0f;
         }
         
-        if((BallPosition.y >= (GetScreenHeight() - 10)) || (BallPosition.y <= 10)){
+        if((BallPosition.y >= (GetScreenHeight() - 10)) || (BallPosition.y <= 10)){ // bounce the ball in the oposite direction
+            BallSpeed.y *= -1.0f;
+        }
+        
+        if(Collition){
+            BallSpeed.x *= -1.0f;
             BallSpeed.y *= -1.0f;
         }
                 
-        if(BallPosition.y >= (GetScreenHeight() - 10)){
+        if(BallPosition.y >= (GetScreenHeight() - 10)){ // stops the ball when it hits the bottom edge
             BallSpeed.x = 0;
             BallSpeed.y = 0;
         }
         
-        if(IsKeyPressed(KEY_W)){
+        if(IsKeyPressed(KEY_W)){ // bounce the ball again if it stops
             BallSpeed.x = 5;
             BallSpeed.y = -5;
         }
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
         
         BeginDrawing();
         {
             ClearBackground(RAYWHITE);
 
-            DrawCircle( BallPosition.x, BallPosition.y, 5, BLACK);
+            //DrawCircle( BallPosition.x, BallPosition.y, 5, BLACK);
+            DrawRectangleRec(Ball, BLACK);
             
-            DrawRectangle( BordPosition.x, BordPosition.y, 150, 30, WHITE);
+            //DrawRectangle( BordPosition.x, BordPosition.y, 150, 30, WHITE);
+            DrawRectangleRec(Bord, WHITE);
             DrawRectangleLines( BordPosition.x, BordPosition.y, 150, 30, BLACK);
+            
         }    
         EndDrawing();
         //----------------------------------------------------------------------------------
