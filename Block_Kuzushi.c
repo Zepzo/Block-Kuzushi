41c59fbd7ebce6b37dd891000bf054d2d322fd41
Zätha <zathamalmberg@gmail.com>
2025-01-22 14:58:52 +0100

    The game has a game loop

--- Block_Kuzushi.c
+++ Block_Kuzushi.c
@@ -14,7 +14,10 @@ typedef struct Box{
     float width;           
     float height;
     bool alive;
+    int Hp;
 }Box;
+
+typedef enum GameScreen {Main = 0, level_1, level_2, level_3, GameOver, WinScreen} GameScreen;
 
 
 int main(void)
@@ -24,7 +27,7 @@ int main(void)
     const int screenWidth = 1000;
     const int screenHeight = 650;
     
-    int AmountOfBoxes = 20;
+    int AmountOfBoxes = 10;
     
     Box Boxes[AmountOfBoxes];
     
@@ -34,6 +37,7 @@ int main(void)
         Boxes[i].width = 70;
         Boxes[i].height = 30;
         Boxes[i].alive = true;
+        Boxes[i].Hp = 1;
     }
             
     Vector2 BordPosition = { 450, 600};
@@ -51,6 +55,10 @@ int main(void)
     
     bool exitWindowRequested = false;   // Flag to request window to exit
     bool exitWindow = false;    // Flag to set window to exit
+    
+    int WinGame = 0;
+    
+    GameScreen CurenScreen = Main;
             
     InitWindow(screenWidth, screenHeight, "My block kuzushi game");
 
@@ -64,54 +72,133 @@ int main(void)
         // Update
         //----------------------------------------------------------------------------------
         
-        if(!exitWindowRequested){
-             if(BordPosition.x > 0){ // moves the board
-                if (IsKeyDown(KEY_A)){
-                    BordPosition.x -= 6;
-                }
-            }
-            
-            if(BordPosition.x < 850){ // moves the board
-                if (IsKeyDown(KEY_D)){
-                    BordPosition.x += 6;
-                }
-            }
-        
-            BallPosition.x += BallSpeed.x;
-            BallPosition.y += BallSpeed.y;
-            
-            if((BallPosition.x >= (GetScreenWidth() - BallRadius)) || (BallPosition.x <= BallRadius)){ // bounce the ball in the oposite direction
-                BallSpeed.x *= -1.0f;
-            }
-            
-            if((BallPosition.y >= (GetScreenHeight() - BallRadius)) || (BallPosition.y <= BallRadius)){ // bounce the ball in the oposite direction
-                BallSpeed.y *= -1.0f;
-            }
-                    
-            if(BallPosition.y >= (GetScreenHeight() - BallRadius)){ // stops the ball when it hits the bottom edge
-                BallSpeed.x = 0;
-                BallSpeed.y = 0;
-            }
-            
-            //Bounce the ball of the boxes
-            for(int i = 0; i < AmountOfBoxes; i++){
-                if(Boxes[i].alive){
-                    if(BallPosition.y >= Boxes[i].y && BallPosition.y <= Boxes[i].y + Boxes[i].height && BallPosition.x >= Boxes[i].x && BallPosition.x <= Boxes[i].x + Boxes[i].width){
-                        BallSpeed.y *= -1.0f;
-                        Boxes[i].alive = false;
+        switch(CurenScreen)
+        {
+            case Main:
+            {
+                if(IsKeyPressed(KEY_SPACE)){ 
+                    CurenScreen = level_1;
+                } 
+            }break;
+            case level_1:
+            {
+                if(!exitWindowRequested){
+                 if(BordPosition.x > 0){ // moves the board
+                    if (IsKeyDown(KEY_A)){
+                        BordPosition.x -= 6;
                     }
                 }
-            }
+                
+                if(BordPosition.x < 850){ // moves the board
+                    if (IsKeyDown(KEY_D)){
+                        BordPosition.x += 6;
+                    }
+                }
             
-            //The Ball abounce of the bord
-            if(BallPosition.y >= BordPosition.y && BallPosition.x >= BordPosition.x && BallPosition.x <= BordPosition.x + BordWidth){
-                BallSpeed.y *= -1.0f;
+                BallPosition.x += BallSpeed.x;
+                BallPosition.y += BallSpeed.y;
+                
+                if((BallPosition.x >= (GetScreenWidth() - BallRadius)) || (BallPosition.x <= BallRadius)){//bounce the ball in the oposite direction
+                    BallSpeed.x *= -1.0f;
+                }
+                
+                if((BallPosition.y >= (GetScreenHeight() - BallRadius)) || (BallPosition.y <= BallRadius)){ // bounce the ball in the oposite direction
+                    BallSpeed.y *= -1.0f;
+                }
+                        
+                if(BallPosition.y >= (GetScreenHeight() - BallRadius)){ // stops the ball when it hits the bottom edge
+                    WinGame = 0;
+                    CurenScreen = GameOver;
+                }
+                
+                if(WinGame == 10){
+                                WinGame = 0;
+                                CurenScreen = WinScreen;
+                            }
+                
+                //Bounce the ball of the boxes
+                for(int i = 0; i < AmountOfBoxes; i++){
+                    if(Boxes[i].alive){
+                        if(BallPosition.y >= Boxes[i].y && BallPosition.y <= Boxes[i].y + Boxes[i].height && BallPosition.x >= Boxes[i].x && BallPosition.x <= Boxes[i].x + Boxes[i].width){
+                            BallSpeed.y *= -1.0f;
+                            Boxes[i].alive = false;
+                            WinGame++;
+                        }
+                    }
+                }
+                
+                //The Ball abounce of the bord
+                if(BallPosition.y >= BordPosition.y && BallPosition.x >= BordPosition.x && BallPosition.x <= BordPosition.x + BordWidth){
+                    BallSpeed.y *= -1.0f;
+                    BallSpeed.x = (BallPosition.x - BordPosition.x)*5;
+
+                }
+                
+                if(IsKeyPressed(KEY_W)){ // bounce the ball again if it stops
+                    BallSpeed.x = 5;
+                    BallSpeed.y = -5;
+                }   
             }
-            
-            if(IsKeyPressed(KEY_W)){ // bounce the ball again if it stops
-                BallSpeed.x = 5;
-                BallSpeed.y = -5;
-            }   
+            }break;
+            case level_2:
+            {
+                
+            }break;
+            case level_3:
+            {
+                
+            }break;
+            case GameOver:
+            {
+                if(IsKeyPressed(KEY_SPACE)){ 
+                
+                    for(int i = 0; i < AmountOfBoxes; i++){
+                        Boxes[i].x = 10.0f + 90.0f *(i%10) + 10.0f *(i%10);
+                        Boxes[i].y = 20.0f + 90.0f *(i/10) + 10.0f *(i/10);
+                        Boxes[i].width = 70;
+                        Boxes[i].height = 30;
+                        Boxes[i].alive = true;
+                        Boxes[i].Hp = 1;
+                    }
+                    
+                    BordPosition.x = 450;
+                    BordPosition.y = 600;
+                    
+                    BallPosition.x = 525;
+                    BallPosition.y = 570;
+                    
+                    BallSpeed.x = -5;
+                    BallSpeed.y = -5;
+                
+                    CurenScreen = level_1;
+                }
+            }break;
+            case WinScreen:
+            {
+                if(IsKeyPressed(KEY_SPACE)){ 
+                
+                    for(int i = 0; i < AmountOfBoxes; i++){
+                        Boxes[i].x = 10.0f + 90.0f *(i%10) + 10.0f *(i%10);
+                        Boxes[i].y = 20.0f + 90.0f *(i/10) + 10.0f *(i/10);
+                        Boxes[i].width = 70;
+                        Boxes[i].height = 30;
+                        Boxes[i].alive = true;
+                        Boxes[i].Hp = 1;
+                    }
+                    
+                    BordPosition.x = 450;
+                    BordPosition.y = 600;
+                    
+                    BallPosition.x = 525;
+                    BallPosition.y = 570;
+                    
+                    BallSpeed.x = -5;
+                    BallSpeed.y = -5;
+                
+                    CurenScreen = level_1;
+                }
+            }break;
+            default: break;
         }
         
         // Detect if X-button or KEY_ESCAPE have been pressed to close window
@@ -133,24 +220,53 @@ int main(void)
         BeginDrawing();
         {
             ClearBackground(RAYWHITE);
-
-            DrawCircle( BallPosition.x, BallPosition.y, BallRadius, BallColor);
             
-            DrawRectangle( BordPosition.x, BordPosition.y, BordWidth, BordHight, BordColor);
-            DrawRectangleLines( BordPosition.x, BordPosition.y, BordWidth, BordHight, BordFrame);
-            
-            for(int i = 0; i < AmountOfBoxes; i++){
-                if(Boxes[i].alive){
-                 DrawRectangle(Boxes[i].x, Boxes[i].y, Boxes[i].width, Boxes[i].height, GREEN);   
-                }
-            }
-            
-            if (exitWindowRequested)
+        switch(CurenScreen)
+        {
+            case Main:
             {
-                DrawRectangle(0, 100, screenWidth, 200, BLACK);
-                DrawText("Are you sure you want to exit program? [Y/N]", 40, 180, 30, WHITE);
-            }
+                DrawRectangle(0, 0, screenWidth, screenHeight, BLACK);
+                DrawText("To start the game press [Space]", 40, 280, 50, WHITE);
+            }break;
+            case level_1:
+            {
+                DrawCircle( BallPosition.x, BallPosition.y, BallRadius, BallColor);
             
+                DrawRectangle( BordPosition.x, BordPosition.y, BordWidth, BordHight, BordColor);
+                DrawRectangleLines( BordPosition.x, BordPosition.y, BordWidth, BordHight, BordFrame);
+                
+                for(int i = 0; i < AmountOfBoxes; i++){
+                    if(Boxes[i].alive){
+                     DrawRectangle(Boxes[i].x, Boxes[i].y, Boxes[i].width, Boxes[i].height, GREEN);   
+                    }
+                }
+            }break;
+            case level_2:
+            {
+                
+            }break;
+            case level_3:
+            {
+                
+            }break;
+            case GameOver:
+            {
+                DrawRectangle(0, 0, screenWidth, screenHeight, BLACK);
+                DrawText("GameOver press [Space] to restart", 40, 280, 50, WHITE);
+            }break;
+            case WinScreen:
+            {
+                DrawRectangle(0, 0, screenWidth, screenHeight, BLACK);
+                DrawText("You win, press [Space] to restart", 40, 280, 50, WHITE);
+            }break;
+            default: break;
+        }
+        
+        if (exitWindowRequested)
+                {
+                    DrawRectangle(0, 0, screenWidth, screenHeight, BLACK);
+                    DrawText("Are you sure you want to exit program? [Y/N]", 40, 180, 30, WHITE);
+                }
         }    
         EndDrawing();
         //----------------------------------------------------------------------------------
